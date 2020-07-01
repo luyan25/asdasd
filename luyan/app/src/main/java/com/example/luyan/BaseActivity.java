7b9a953ca8b8b32362e5768ad1eb51233a007e81
luyan <2676900677@qq.com>
2020-07-01 21:12:44 +0800

    hjkhjh

---
+++ luyan/app/src/main/java/com/example/luyan/BaseActivity.java
@@ -0,0 +1,11 @@
+package com.example.luyan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public abstract class BaseActivity extends AppCompatActivity {
+
+    void initView(){};
+
+    void initData(){};
+
+}
